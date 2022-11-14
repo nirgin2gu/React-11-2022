a8950df6684f87aa6a5bc65f40565e7291edcd41
nirgin2gu <prigotauri@gmail.com>
2022-11-14 18:16:34 +0200

    Update Seaded.js

--- veebipood/src/pages/Seaded.js
+++ veebipood/src/pages/Seaded.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 
 function Seaded() {
-    const [keel, uuendaKeel] = useState("est");
+    const [keel, uuendaKeel] = useState(localStorage.getItem("keel"));
+    
+    const muudaKeelEst = () => {
+        uuendaKeel("est");
+        localStorage.setItem("keel", "est");
+    }
+    const muudaKeelEng = () => {
+        uuendaKeel("eng");
+        localStorage.setItem("keel", "eng");
+    }
 
+    const muudaKeelRus = () => {
+        uuendaKeel("rus");
+        localStorage.setItem("keel", "eng");
+    }
 
     return ( 
     <div>
-        <button onClick={() => uuendaKeel("est")}>Eesti</button>
-        <button onClick={() => uuendaKeel("eng")}>Inglise</button>
-        <button onClick={() => uuendaKeel("rus")}>Vene</button>
-
-
+        <button onClick={muudaKeelEst}>Eesti</button>
+        <button onClick={muudaKeelEng}>Inglise</button>
+        <button onClick={muudaKeelEng}>Inglise</button>
         
         { keel ==="est" &&    <div>Lehekülg on eesti keelne</div> }
         { keel ==="eng" &&   <div>Lehekülg on inglise keelne</div> }
@@ -20,3 +31,8 @@ function Seaded() {
 }
 
 export default Seaded;
+
+//SALVESTATAKSE 
+// 1. Andmebaas -> kõik tundlikud andmed, andmed mida peab kõigile näitama(passwords etc)
+// 2. Browseri vahemällu -> lehekülje seadistused(keelevalik, dark mode/light mode, tooteid valides infinite scroll/leheküljenumbrid all)
+// 3. Faili -> logid / mis rakenduses toimub / analüütika (kuhu klikid jne, miks ei toimi)
